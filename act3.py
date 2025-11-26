b12f6253de4158fc09685342d86ac900c4e00b9a
Jose <josepedrosvilches07@gmail.com>
2025-11-26 13:48:51 +0100

    Activitats UD2 26-11-2025

--- act3.py
+++ act3.py
@@ -3,9 +3,9 @@ contar = 0
 while True:
     contar= int(input("Introdueixme un numero (0 pa acabar):"))
 
-    if contar == 0
+    if contar == 0:
         break
-    elif contar > 0
+    elif contar > 0:
         contar +=1
 
 print(("Has introduit {Contar} numeros positius"))
