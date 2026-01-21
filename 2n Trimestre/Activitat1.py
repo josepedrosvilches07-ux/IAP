52c135ca3f00f1af58ceaca212fd5040e8684d9f
Jose12_07 <josepedrosvilches07@gmail.com>
2026-01-21 15:45:23 +0100

    Activitats 19/01/2026

---
+++ 2n Trimestre/Activitat1.py
@@ -0,0 +1,32 @@
+import numpy as np
+
+print("--- Exercisi 1 ---")
+
+vector = np.array([23,25,19,30,21])
+print(vector)
+
+print("--- Exercici 2 ---")
+
+matriu2d = np.array([
+    [1, 2, 3],
+    [4, 5, 6]
+])
+
+print(matriu2d)
+
+print("--- Exercici 3 ---")
+
+matriu3d = np.array([
+    [[1, 2], [3, 4]],
+    [[5, 6], [7, 8]],
+    [[9, 10], [11, 12]]
+])
+print(matriu3d)
+
+print("--- Exercici 4 ---")
+
+print(vector[0])
+
+print(matriu2d[1])
+
+print(matriu3d[1, 0, 1])
